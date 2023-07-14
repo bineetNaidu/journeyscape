8d4ba3e06397c7220076c5d909ce911497cfe872
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-07-14 14:02:03 +0530

    🔢 UX updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,36 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { Poppins } from 'next/font/google';
+import { Poppins, Cookie } from 'next/font/google';
+import Head from 'next/head';
 
 const fonts = Poppins({
   subsets: ['latin'],
   weight: ['400', '700', '100', '300', '500', '900', '200', '600', '800'],
 });
 
+const brandFont = Cookie({
+  subsets: ['latin'],
+  weight: ['400'],
+});
+
 export default function Home() {
   return (
     <main
-      className={`${fonts.className} bg-[#b0d8e4] relative min-h-screen h-full`}
+      className={`${fonts.className} bg-[#b0d8e4] relative min-h-screen h-full overflow-hidden`}
     >
-      <nav className="flex justify-between items-center px-5">
-        <div className="relative">
-          <Image
-            src="/logo.svg"
-            alt="Logo"
-            className="w-[300px]"
-            width={100}
-            height={100}
-            priority
-          />
-        </div>
+      <Head>
+        <title>Journeyscape</title>
+        <meta
+          name="description"
+          content="Embark on Extraordinary Journeys with Journeyscape."
+        />
+      </Head>
+      <nav className="flex justify-between items-center px-5 md:px-20 py-8">
+        <Link href="/">
+          <h1 className={`${brandFont.className} text-orange-500 text-4xl`}>
+            Journeyscape
+          </h1>
+        </Link>
         <div className="hidden flex-1 md:flex justify-center gap-6">
           <Link href="/">
             <span className="font-bold hover:text-orange-500 transition-all">
