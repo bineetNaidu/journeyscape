d54c1cf6f2215120133fb1b48533ab6f2b6417e6
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-07-20 23:21:16 +0530

    🔥add mob hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,39 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FC } from 'react';
 import { Button } from './Button';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { Separator } from '@/components/ui/separator';
 
 const brandFont = Cookie({
   subsets: ['latin'],
   weight: ['400'],
 });
+
+const links = [
+  {
+    name: 'Home',
+    href: '/',
+  },
+  {
+    name: 'About us',
+    href: '/about-us',
+  },
+  {
+    name: 'Destinations',
+    href: '/destinations',
+  },
+  {
+    name: 'Features',
+    href: '/product',
+  },
+];
 
 export const Navbar: FC = () => {
   const router = useRouter();
@@ -22,45 +50,65 @@ export const Navbar: FC = () => {
         </h1>
       </Link>
       <div className="hidden flex-1 md:flex justify-center gap-6">
-        <Link href="/">
-          <span
-            className={`hover:text-orange-500 transition-all ${
-              router.pathname === '/' && 'font-bold'
-            }`}
-          >
-            Home
-          </span>
-        </Link>
-        <Link href="/about-us">
-          <span
-            className={`hover:text-orange-500 transition-all ${
-              router.pathname === '/about-us' && 'font-bold'
-            }`}
-          >
-            About us
-          </span>
-        </Link>
-        <Link href="/destinations">
-          <span
-            className={`hover:text-orange-500 transition-all ${
-              router.pathname === '/destinations' && 'font-bold'
-            }`}
-          >
-            Destinations
-          </span>
-        </Link>
-        <Link href="/product">
-          <span
-            className={`hover:text-orange-500 transition-all ${
-              router.pathname === '/product' && 'font-bold'
-            }`}
-          >
-            Features
-          </span>
-        </Link>
+        {links.map((link) => (
+          <Link href={link.href} key={link.name}>
+            <span
+              className={`hover:text-orange-500 transition-all ${
+                router.pathname === link.href && 'font-bold'
+              }`}
+            >
+              {link.name}
+            </span>
+          </Link>
+        ))}
       </div>
       <div>
-        <Link href="/signup">
+        <Sheet>
+          <SheetTrigger asChild>
+            <div className="group md:hidden flex h-12 w-12 cursor-pointer items-center justify-center rounded-3xl bg-transparent p-2 hover:bg-slate-200">
+              <div className="space-y-2">
+                <span className="block h-1 w-8 origin-center rounded-full bg-slate-500 transition-transform ease-in-out group-hover:translate-y-1.5 group-hover:rotate-45"></span>
+                <span className="block h-1 w-6 origin-center rounded-full bg-orange-500 transition-transform ease-in-out group-hover:w-8 group-hover:-translate-y-1.5 group-hover:-rotate-45"></span>
+              </div>
+            </div>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>
+                <Link href="/">
+                  <h1
+                    className={`${brandFont.className} text-orange-500 text-4xl font-bold`}
+                  >
+                    Journeyscape
+                  </h1>
+                </Link>
+              </SheetTitle>
+              <SheetDescription>
+                <p>~ Ignite Your Wanderlust with Journeyscape.</p>
+              </SheetDescription>
+            </SheetHeader>
+            <Separator className="my-5" />
+            <div className="flex flex-col gap-4 my-4">
+              {links.map((link) => (
+                <Link href={link.href} key={link.name}>
+                  <span
+                    className={`hover:text-orange-500 transition-all ${
+                      router.pathname === link.href && 'font-bold'
+                    } text-slate-5`}
+                  >
+                    {link.name}
+                  </span>
+                </Link>
+              ))}
+              <Separator />
+              <Link href="/signup">
+                <Button>Sign up</Button>
+              </Link>
+            </div>
+          </SheetContent>
+        </Sheet>
+
+        <Link href="/signup" className="hidden md:block">
           <Button>Sign up</Button>
         </Link>
       </div>
