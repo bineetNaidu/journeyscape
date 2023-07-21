0773b3dba5c16349eb80ce079813574372316168
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-07-21 17:37:05 +0530

    🎉 add graphql api

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'plus.unsplash.com'],
   },
+  webpack: (config) => {
+    config.experiments = config.experiments || {};
+    config.experiments.topLevelAwait = true;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
